9d55ab661735f76f7393040dce80bf84f8b60d07
Masafumi Yokoyama <myokoym@gmail.com>
2014-12-06 22:35:55 +0900

    Start

---
+++ crawler.rb
@@ -0,0 +1,62 @@
+#!/usr/bin/env ruby
+
+require "fileutils"
+require "net/http"
+require "uri"
+
+HOST_NAME = "http://sourceforge.jp/"
+BASE_PATH = "projects/groonga/lists/archive/dev/"
+
+YEARS = [
+  "2009",
+  "2010",
+  "2011",
+  "2012",
+  "2013",
+  "2014",
+]
+
+MONTHS = [
+  "January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+]
+
+FileUtils.mkdir_p("data")
+
+YEARS.product(MONTHS) do |pair|
+  year, month = *pair
+  basename = "#{year}-#{month}.txt"
+  output_path = File.join("data", basename)
+  if File.exist?(output_path)
+    $stderr.puts("#{basename} is already exists.")
+    next
+  end
+
+  uri = URI.parse("#{HOST_NAME}#{BASE_PATH}#{basename}")
+  res = nil
+  5.times do
+    res = Net::HTTP.start(uri.host, uri.port) do |http|
+      http.get(uri.request_uri)
+    end
+
+    case res
+    when Net::HTTPSuccess
+      break
+    when Net::HTTPRedirection
+      uri = URI.parse(res["Location"])
+      next
+    else
+      break
+    end
+  end
+  sleep 0.5
+  unless res.is_a?(Net::HTTPSuccess)
+    $stderr.puts("#{basename} is not found.")
+    next
+  end
+
+  File.open(output_path, "w") do |input_file|
+    euc_text = res.body
+    input_file.write(euc_text.encode("UTF-8", "EUC-JP"))
+  end
+end
