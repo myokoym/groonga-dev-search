697332530644a88fd0a6f597f1d6b31f0435b720
Masafumi Yokoyama <myokoym@gmail.com>
2014-12-07 01:09:20 +0900

    indexer: Use mail gem

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source "https://rubygems.org/"
 
 gem "grn_mini"
+gem "mail"
--- indexer.rb
+++ indexer.rb
@@ -1,5 +1,6 @@
 require "grn_mini"
 require "fileutils"
+require "mail"
 
 require "./config"
 
@@ -8,40 +9,30 @@ FileUtils.mkdir_p(DB_DIR)
 db_path = File.join(DB_DIR, "groonga-dev-search.db")
 
 GrnMini.create_or_open(db_path)
+hash = GrnMini::Hash.new
 
 Dir.glob("#{DATA_DIR}/*") do |path|
   index_by_month = 0
   basename = File.basename(path, ".txt")
   year, month = basename.split(/-/)
   File.open(path) do |file|
-    key = nil
-    hash = nil
-    data = nil
-    body = nil
-    mode = :head
+    text = ""
     file.each_line do |line|
       case line
       when /^From /
-        data[:body] = body if body
-        hash[key] = data if data
-
+        next if text.empty?
+        mail = Mail.new(text)
+        text = ""
         index_by_month += 1
         key = "#{basename}-#{"%04d" % index_by_month}"
-        hash = GrnMini::Hash.new
-        data = {}
-        body = ""
-        mode = :head
-      when /^From: (.*)/
-        data[:from] = $1
-      when /^Date: (.*)/
-        data[:date] = $1
-      when /^Subject: (.*)/
-        data[:subject] = $1
-      when /^Message-ID: (.*)/
-        data[:"message-id"] = $1
-        mode = :body
+        hash[key] = {
+          :from    => mail.from,
+          :date    => mail.date.to_s,
+          :subject => mail.subject,
+          :body    => mail.body.to_s.force_encoding("UTF-8"),
+        }
       else
-        body << line
+        text << line
       end
     end
   end
