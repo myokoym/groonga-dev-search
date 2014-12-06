0edf6db4177c8b5e71a3571f5a360924f4e020b0
Masafumi Yokoyama <myokoym@gmail.com>
2014-12-06 22:49:25 +0900

    Extract common code

---
+++ common.rb
@@ -0,0 +1,16 @@
+HOST_NAME = "http://sourceforge.jp/"
+BASE_PATH = "projects/groonga/lists/archive/dev/"
+
+YEARS = [
+  "2009",
+  "2010",
+  "2011",
+  "2012",
+  "2013",
+  "2014",
+]
+
+MONTHS = [
+  "January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+]
--- crawler.rb
+++ crawler.rb
@@ -4,22 +4,7 @@ require "fileutils"
 require "net/http"
 require "uri"
 
-HOST_NAME = "http://sourceforge.jp/"
-BASE_PATH = "projects/groonga/lists/archive/dev/"
-
-YEARS = [
-  "2009",
-  "2010",
-  "2011",
-  "2012",
-  "2013",
-  "2014",
-]
-
-MONTHS = [
-  "January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
-]
+require "./common"
 
 FileUtils.mkdir_p("data")
 
